4d322f5dae68c220e9bd24298a847e87722d76d1
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-14 19:18:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const byteSize = (str) => {
   // write your code here
+	
+	console.log(byteSize('hello world')); // 11
+console.log(byteSize('안녕하세요'));   // 15
+console.log(byteSize(''));
 };
 
 // Do not change the code below
